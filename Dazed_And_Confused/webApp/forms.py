f759cad3a0022633916c3ce68e89e89585ff7413
haleyt <haleyt@wit.edu>
2018-04-02 19:08:41 -0400

    fixed registration page

--- Dazed_And_Confused/webApp/forms.py
+++ Dazed_And_Confused/webApp/forms.py
@@ -4,11 +4,6 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length = 50)
-    email = forms.EmailField(max_length=254, help_text=None)
-    password1 = forms.CharField(max_length=39, help_text=None, widget=forms.PasswordInput())
-    password2 = forms.CharField(max_length=39, help_text=None, widget=forms.PasswordInput())
-
 
     class Meta:
         model = User
