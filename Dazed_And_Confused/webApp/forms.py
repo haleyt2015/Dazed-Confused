43da452de14d6e21d2e8c8faff388da95a8f51c1
haleyt <haleyt@wit.edu>
2018-04-05 12:24:55 -0400

    Added model form import to views.py

--- Dazed_And_Confused/webApp/forms.py
+++ Dazed_And_Confused/webApp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import models
+from django.forms import ModelForm
 
 
 class SignUpForm(UserCreationForm):
